fb95f4c83b6182726511a2f6a1f70db4cd601d4d
happymiks <112277887+happymiks@users.noreply.github.com>
2022-08-30 22:16:48 +0300

    commit

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    void Awake() { instance = this; }
+
+    void Start()
+    {
+        StartCoroutine(WaveStartDelay());
+    }
+
+    IEnumerator WaveStartDelay()
+    {
+        //wait for X seconds
+        yield return new WaitForSeconds(2f);
+        //Start the enemy spawning
+        GetComponent<EnemySpawner>().StartSpawning();
+    }
+}
