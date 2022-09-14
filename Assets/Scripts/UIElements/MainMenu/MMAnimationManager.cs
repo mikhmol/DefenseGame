7c2cce7d43c4b8190ab5eb079a66d93bb50c7b17
Dynam1k <dynam1ks@ukr.net>
2022-09-14 13:58:53 +0300

    added rus-su anim controller script

---
+++ Assets/Scripts/UIElements/MainMenu/MMAnimationManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MMAnimationManager : MonoBehaviour
+{
+    private Animator planesAnimation;
+
+    int randomAnim = 0, prevAnim = 0;
+
+    IEnumerator Start()
+    {
+        planesAnimation = GetComponent<Animator>();
+
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+
+            while (randomAnim == prevAnim) 
+            {
+                randomAnim = Random.Range(0, 10);
+            }
+
+            prevAnim = randomAnim;
+            planesAnimation.SetInteger("PlaneIndex", randomAnim);
+            planesAnimation.SetTrigger("Plane");
+        }
+    }
+}
