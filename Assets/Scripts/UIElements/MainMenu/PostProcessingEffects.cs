4b66379442514a7ba006064a67f0180e28df5ba0
Dynam1k <dynam1ks@ukr.net>
2022-09-15 11:31:40 +0300

    PP script

--- Assets/Scripts/UIElements/MainMenu/PostProcessingEffects.cs
+++ Assets/Scripts/UIElements/MainMenu/PostProcessingEffects.cs
@@ -8,33 +8,59 @@ public class PostProcessingEffects : MonoBehaviour
     private PostProcessVolume ppVol;
     private Bloom bloom;
     private Vignette vignette;
-    private ChromaticAberration chromAberration;
+    private ChromaticAberration chromAber;
 
-    private void Awake()
-    {
-        ppVol = GetComponent<PostProcessVolume>();
-        ppVol.profile.TryGetSettings(out chromAberration);
-        ppVol.profile.TryGetSettings(out bloom);
-        ppVol.profile.TryGetSettings(out vignette);
-    }
+    // animation curves for different PP effects
+    [SerializeField] private AnimationCurve chromAberIntensity;
+    [SerializeField] private AnimationCurve bloomIntensity;
+    [SerializeField] private AnimationCurve vignetteIntensity;
+
+    // variables for looping the animation
+    private float[] currentTime = new float[3];
+    private float[] totalTime = new float[3];
 
     private void Start()
     {
-        //chromAberration.intensity.value = 1f;
-        //StartCoroutine(chromAberrationOnLoad());
+        // initializing post processing variables
+        ppVol = GetComponent<PostProcessVolume>();
+        ppVol.profile.TryGetSettings(out chromAber);
+        ppVol.profile.TryGetSettings(out bloom);
+        ppVol.profile.TryGetSettings(out vignette);
+
+        totalTime[0] = chromAberIntensity.keys[chromAberIntensity.keys.Length - 1].time;
+        totalTime[1] = bloomIntensity.keys[bloomIntensity.keys.Length - 1].time;
+        totalTime[2] = vignetteIntensity.keys[vignetteIntensity.keys.Length - 1].time;
     }
 
     private void Update()
     {
-        
-    }
-
-    /*IEnumerator chromAberrationOnLoad()
-    {
-        while (chromAberration.intensity.value != 0)
+        // chromatic aberration curve loop
+        if (currentTime[0] < totalTime[0])
         {
-            chromAberration.intensity.value -= 0.05f;
-            yield return new WaitForSeconds(0.03f);
+            chromAber.intensity.value = chromAberIntensity.Evaluate(currentTime[0]);
+            currentTime[0] += Time.deltaTime;
         }
-    }*/
+        else
+        {
+            chromAber.intensity.value = 0;
+        }
+
+        // bloom curve loop
+        bloom.intensity.value = bloomIntensity.Evaluate(currentTime[1]);
+        currentTime[1] += Time.deltaTime;
+
+        if (currentTime[1] >= totalTime[1])
+        {
+            currentTime[1] = 0;
+        }
+
+        // vignette curve loop
+        vignette.intensity.value = vignetteIntensity.Evaluate(currentTime[2]);
+        currentTime[2] += Time.deltaTime;
+
+        if (currentTime[2] >= totalTime[2])
+        {
+            currentTime[2] = 0;
+        }
+    }
 }
