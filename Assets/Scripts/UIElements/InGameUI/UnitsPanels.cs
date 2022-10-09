d44a446f0ca7c2f6d4b0b3c408f3413fa7ac3504
Dynam1k <dynam1ks@ukr.net>
2022-10-09 20:20:39 +0300

    added static unit menu and open anims

---
+++ Assets/Scripts/UIElements/InGameUI/UnitsPanels.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UnitsPanels : MonoBehaviour
+{
+    public RectTransform personnelClassPanel, vehiclesClassPanel, facilitiesClassPanel;
+
+    private float xDefPos = 760f, yDefPos = -45f, yPos = -0.5f;
+
+    private void Start()
+    {
+        personnelClassPanel.DOAnchorPos(new Vector2(xDefPos, yPos), 0.25f);
+    }
+
+    public void OpenClassButton(int classID)
+    {
+        if(classID == 0)
+        {
+            OpenClassPanel(personnelClassPanel);
+        }
+        else if(classID == 1)
+        {
+            OpenClassPanel(vehiclesClassPanel);
+        }
+        else if(classID == 2)
+        {
+            OpenClassPanel(facilitiesClassPanel);
+        }
+    }
+
+    private void OpenClassPanel(RectTransform panel)
+    {
+        vehiclesClassPanel.DOAnchorPos(new Vector2(xDefPos, yDefPos), 0.25f);
+        facilitiesClassPanel.DOAnchorPos(new Vector2(xDefPos, yDefPos), 0.25f);
+        personnelClassPanel.DOAnchorPos(new Vector2(xDefPos, yDefPos), 0.25f);
+
+        panel.DOAnchorPos(new Vector2(xDefPos, yPos), 0.25f).SetDelay(0.25f);
+    }
+}
