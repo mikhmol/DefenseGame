f5b05be71e5cbec83c4c89c1f82d32576808b802
mikhmol <mikhailo.moliakov@gmail.com>
2022-10-14 03:06:11 +0300

    Added TileData

--- Assets/Scripts/GameMechanics/TowerMovement.cs
+++ Assets/Scripts/GameMechanics/TowerMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -8,12 +8,26 @@ public class TowerMovement : MonoBehaviour
     [SerializeField] Tilemap map;
     [SerializeField] private static List<TowerMovement> moveToMice = new List<TowerMovement>();
     [SerializeField] private float _speed;
+    [SerializeField] private List<TileData> tileDatas;
+    private Dictionary<TileBase, TileData> dataFromTiles;
     private Vector3 _target;
     public Vector3Int gridPosition;
     public Vector3 tileCenter;
     private int tileId;
 
     private bool _selected;
+
+    private void Awake()
+    {
+        dataFromTiles = new Dictionary<TileBase, TileData>();
+        foreach (var tileData in tileDatas)
+        {
+            foreach (var tile in tileData.tiles)
+            {
+                dataFromTiles.Add(tile, tileData);
+            }
+        }
+    }
 
     void Start()
     {
@@ -48,9 +62,13 @@ public class TowerMovement : MonoBehaviour
     private void CheckTile()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(mousePosition);
+        //Debug.Log(mousePosition);
         gridPosition = map.WorldToCell(mousePosition);
-        Debug.Log(gridPosition);
+        TileBase clickedTile = map.GetTile(gridPosition);
+        //float walkingSpeed = dataFromTiles[clickedTile].walkingSpeed;
+        //print(clickedTile);
+        //print(walkingSpeed);
+        //Debug.Log(gridPosition);
         if (map.HasTile(gridPosition))
         {
             tileCenter = map.GetCellCenterWorld(gridPosition);
